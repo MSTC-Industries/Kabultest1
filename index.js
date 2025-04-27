d9895229e237174e6c4e4b121bbd7b86d7e1b0fc
MSTC-Industries <mstc.industries.official@gmail.com>
2025-04-27 02:20:20 +0000

    ITS BEAUTIFUL

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 var totalprice = 0;
 var toggle = 0;
+
 function toggleCart() {
     const cartPanel = document.getElementById('cart-panel');
     cartPanel.classList.toggle('open');
 
-    getPrice()
     toggle = toggle + 1;
 }
 
-function getPrice(){
-    if(toggle%2 == 0){
-    var cart = document.createElement("div");
-    cart.id = "cart";
-    /* var ul = document.createElement("ul"); */
+function addToCart(button) {
+    const parentElement = button.parentElement;
+    var cart = document.getElementById("cartItems");
 
-    for(let i = 1;i<9;i++){
-        var count = document.getElementById("quantity"+i);
-        var pv = count.value;
-        var multi = document.getElementById("v"+i);
-        var multiv = multi.getAttribute("value");
-        pv = pv * multiv;
-        pv = Math.round(pv*100)/100;
-        totalprice = totalprice + pv;
-        if(pv != 0){
-            var title = document.getElementById("name"+i)
-            var name = title.innerText;
-            var cartitem = document.createElement("h2");
-            cartitem.innerHTML = name + ": $"+ pv;
-            
-            cart.appendChild(cartitem);
-        }
+    var count = getChildById(parentElement, "quantity");
+    var pv = count.value;
+    var multi = getChildById(parentElement, "v");
+    var multiv = multi.getAttribute("value");
+
+    pv = pv * multiv;
+    pv = Math.round(pv*100)/100;
+    totalprice = totalprice + pv;
+
+    if(pv != 0){
+        var title = getChildById(parentElement, "name");
+        var name = title.innerText;
+        var cartitem = document.createElement("h2");
+        cartitem.innerHTML = name + ": $"+ pv;
+        cart.appendChild(cartitem);
     }
-    var e = document.createElement("h1");
+
+    var e = document.getElementById("totalAmount");
     e.innerHTML = "Total: $"+totalprice;
-    cart.appendChild(e);
-    var putithere = document.getElementById("fortheitems");
-    putithere.appendChild(cart);
-    }
-    else{
-        console.log("he")
-        var del = document.getElementById("cart");
-        del.remove();
-    }
 }
 
+function loadPage(page) {
+    fetch(page)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Page not found");
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById("pagestuff").innerHTML = data;
+      })
+      .catch(error => {
+        console.error(error);
+        document.getElementById("pagestuff").innerHTML = "<p>Error loading page</p>";
+      });
+}
+
+function getChildById(parentElement, id) {
+    const child = parentElement.querySelector(`#${id}`);
+    return child;
+}
